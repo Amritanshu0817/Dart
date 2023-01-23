fd3ac62d0e940b19b723582dd5d19b2bcd8e237d
Amritanshu0817 <asamrit995@gmail.com>
2023-01-23 20:28:31 +0530

    Third commit

---
+++ four.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  List<String> list = [
+    "Pasta | 85",
+    "Red Velvet | 600",
+    "Pizza | 199",
+    "Noodles | 129",
+    "Biryani | 150",
+    "Rice Bowl | 60"
+  ];
+// Pizza is of 199 and the discounted price is ......
+
+  //List<String> separator = list.map((e) => e.trim()).toList();
+  print(list);
+}
--- three.dart
+++ three.dart
@@ -13,4 +13,24 @@ void main(List<String> args) {
 
   myStrList.removeWhere((element) => (element == "Cake"));
   print(myStrList);
+
+  myStrList.forEach((element) {
+    print(element);
+  });
+
+//If we want to perform any operation on the each element of the list
+//we can use the method map as it is a generic type so it can
+//perform many operations regarding different datatypes
+
+  List<String> updatedList = myStrList.map((e) => e + " Food Item").toList();
+  print(updatedList);
+
+  List<int> lengthOfElements = myStrList.map((e) => e.length).toList();
+  print(lengthOfElements);
+
+  String name = "  Amritanshu   |  Shekhar";
+
+  List<String> splitString = name.split("|");
+  List<String> sortedName = splitString.map((e) => e.trim()).toList();
+  print(sortedName);
 }
