ca726baac9fb5bb4600de6a2f94b6bc563378f0a
Amritanshu0817 <asamrit995@gmail.com>
2023-01-28 17:36:20 +0530

    final print

--- four.dart
+++ four.dart
@@ -1,14 +1,18 @@
 void main(List<String> args) {
   List<String> list = [
-    "Pasta | 85",
-    "Red Velvet | 600",
-    "Pizza | 199",
-    "Noodles | 129",
-    "Biryani | 150",
-    "Rice Bowl | 60"
+    "Pasta  |  85",
+    "Red Velvet  |  600",
+    "Pizza  |  199",
+    "Noodles  |  129",
+    "Biryani  |  150",
+    "Rice Bowl  |  60"
   ];
 // Pizza is of 199 and the discounted price is ......
-
-  //List<String> separator = list.map((e) => e.trim()).toList();
-  print(list);
+  List<String> finalList = list.map((e) {
+    String name = e.split(" | ")[0].trim();
+    double price = double.parse(e.split(" | ")[1].trim());
+    double discountedPrice = 0.8 * price;
+    return ("$name is of food price $price and the discounted price is $discountedPrice \n");
+  }).toList();
+  print(finalList);
 }
