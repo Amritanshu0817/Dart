e6b455723c5daf5b7d997ad536fb22e3cd8593a1
Amritanshu0817 <asamrit995@gmail.com>
2023-01-28 20:56:51 +0530

    High order func()

---
+++ five.dart
@@ -0,0 +1,32 @@
+void main(List<String> args) {
+  printChefName(getChefFromDb, 3, 7);
+}
+
+void printChefName(String Function(int id) getChefFromDb, int id, int n) {
+  String name = getChefFromDb(id);
+  print("$name $n");
+}
+
+String getChefFromDb(int id) {
+  switch (id) {
+    case 1:
+      return "Shivesh";
+
+      break;
+
+    case 2:
+      return "Shivam";
+      break;
+
+    case 3:
+      return "Saurabh";
+      break;
+
+    case 4:
+      return "Amritanshu";
+      break;
+
+    default:
+      return "No one is here";
+  }
+}
